28c0b604336a9005f0adedd6bb69bf992697fd21
Issam Sy <57065659+IssamSY@users.noreply.github.com>
2022-08-11 10:16:57 +0100

    Update Black jack Game.py

--- Black jack Game.py
+++ Black jack Game.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python -i
-Acc_input = float(input("The cash you want to play with?"))
+import os
+Acc_input = float(input("The cash you want to play with? "))
+print("---------------------------------")
 Acc_amont = Acc_input
 # dh is my country currency, you can change to yours e.g $ £ € etc.
 while True:
-    print("---------------------------------")
-    StartQ = input("Do you wanna play? Y/N ")
-    if StartQ == "N":
+    StartQ = input("Another round? Y/N ")
+    if StartQ == "N" or StartQ == "n":
         break
     else:
         print("---------------------------------")
@@ -14,8 +14,11 @@ while True:
         Acc_amont -= Bet
         print("Your account : %0.2f dh" %(Acc_amont))
         WinOrLoss = input("Did you won or lost or push? W/L/P ")
-        if WinOrLoss == "W": Acc_amont = Acc_amont + (Bet * 2)
-        elif WinOrLoss == "P": Acc_amont += Bet
-        elif WinOrLoss =="L": Acc_amont += 0 
+        if WinOrLoss == "W" or WinOrLoss == "w": 
+            Acc_amont = Acc_amont + (Bet * 2)
+        elif WinOrLoss == "P" or WinOrLoss == "p": 
+            Acc_amont += Bet
+        elif WinOrLoss =="L" or WinOrLoss == "l": 
+            Acc_amont += 0 
         print("Your account : %0.2f dh" %(Acc_amont))
-    
+    os.system('clear')
