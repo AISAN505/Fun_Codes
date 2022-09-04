68805d9389fbd8e013a6320bf14687cd74c9b09f
Issam Sy <57065659+IssamSY@users.noreply.github.com>
2022-09-04 10:12:28 +0100

    Update One Line Function.cpp

--- One Line Function.cpp
+++ One Line Function.cpp
@@ -2,3 +2,4 @@
 const auto FUNC_NAME = [] (const auto &PARM_NAME) { /* CODE_OF_BLOCK */};
 //Example
 const auto isPositive = [] (const auto &val) { return val > 0 ? "Positive" : "Negative"};
+const auto oneLine = [] (const auto &x) { return x * 2;}; // Works very well at any data type.
