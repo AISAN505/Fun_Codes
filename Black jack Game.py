4d100d5b779a97654a4fb752b685ac163fc2ca6d
Issam Sy <57065659+IssamSY@users.noreply.github.com>
2023-03-06 00:22:21 +0100

    Making the code more beautiful and clear!

--- Black jack Game.py
+++ Black jack Game.py
@@ -1,24 +1,23 @@
 import os
-Acc_input = float(input("The cash you want to play with? "))
+
+Acc_input = float(input("How you want to play with? "))
 print("---------------------------------")
 Acc_amont = Acc_input
 # dh is my country currency, you can change to yours e.g $ £ € etc.
 while True:
-    StartQ = input("Another round? Y/N ")
-    if StartQ == "N" or StartQ == "n":
-        break
+    StartQ = input("Another round (Y/N)? ")
+    # Here the game starts
+    if StartQ == "N" or StartQ == "n": break
     else:
         print("---------------------------------")
         print("Your account : %0.2f dh" %(Acc_amont))
-        Bet = float(input("Put your bet here: "))
+        Bet = float(input("Input your bet: "))
         Acc_amont -= Bet
-        print("Your account : %0.2f dh" %(Acc_amont))
-        WinOrLoss = input("Did you won or lost or push? W/L/P ")
-        if WinOrLoss == "W" or WinOrLoss == "w": 
-            Acc_amont = Acc_amont + (Bet * 2)
-        elif WinOrLoss == "P" or WinOrLoss == "p": 
-            Acc_amont += Bet
-        elif WinOrLoss =="L" or WinOrLoss == "l": 
-            Acc_amont += 0 
-        print("Your account : %0.2f dh" %(Acc_amont))
+        print("Your current balance : %0.2f dh" %(Acc_amont))
+        # If you win, you win :)
+        WinOrLoss = input("Won(W), Lost(L), Push(P)? ")
+        if WinOrLoss == "W" or WinOrLoss == "w": Acc_amont = Acc_amont + (Bet * 2)
+        elif WinOrLoss == "P" or WinOrLoss == "p": Acc_amont += Bet
+        elif WinOrLoss =="L" or WinOrLoss == "l": Acc_amont += 0 
+        print("Your current balance : %0.2f dh" %(Acc_amont))
     os.system('clear')
